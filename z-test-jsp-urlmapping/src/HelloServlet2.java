6620a9498fadd98ae7443f68707cf7d43bfa3836
Tomlin <s710361112@gmail.com>
2021-12-08 11:50:19 +0800

    新增保護jsp、html範例
    範例檔案: z-test-jsp-urlmapping專案中

---
+++ z-test-jsp-urlmapping/src/HelloServlet2.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet2
+ */
+@WebServlet("/HelloServlet2")
+public class HelloServlet2 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("youdontknow");
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
